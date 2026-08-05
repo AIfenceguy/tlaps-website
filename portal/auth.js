234ed921393d6e965788fd3db7544e47708a40a3
AIfenceguy <rtsui.jlconcepts@gmail.com>
2026-08-04 23:22:34 -0700

    Update portal password hash for rtsui.jlconcepts@gmail.com

--- portal/auth.js
+++ portal/auth.js
@@ -16,7 +16,7 @@
   var VALID_USERS = {
     'jcool009@hotmail.com':    '556c91c548097c5f136472289620ec6f4bfc9f9523b1aa9b9893a682f711bf67',
     'raypeng1118@hotmail.com': '102c7a6f67bd6f4718395ce080d9f95c848c78c334a437c664d992ad53528ae0',
-    'rtsui.jlconcepts@gmail.com': '2f18c5013a6151c7857c7e17f8729b04110d5e2530d6ba2a64fdf91c39758485'
+    'rtsui.jlconcepts@gmail.com': 'add4adbe5cf345eb5013e76eb432fedb38ee9dc39917e701bbc83c7dad03a344'
   };
 
   var AUTH_KEY  = 'tlapsAuth';
